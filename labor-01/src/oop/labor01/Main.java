86bc26605ae9423f8a7f435a53d18004abba39ec
Csipor Antal <tonicsipor@gmail.com>
2022-02-17 15:51:18 +0200

    labor1

---
+++ labor-01/src/oop/labor01/Main.java
@@ -0,0 +1,11 @@
+package oop.labor01;
+
+public class Main {
+    public static void main(String[] args) {
+        String nev = "Csipor Antal";
+        for (int i=0; i<nev.lenght();++i){
+            System.out.println(nev.charAt(i));
+        }
+
+    }
+}

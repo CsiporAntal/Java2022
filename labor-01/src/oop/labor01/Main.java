d54d5bd445fcc1085216270af09a7708c908bc85
CsiporAntal <99884681+CsiporAntal@users.noreply.github.com>
2022-02-24 15:55:07 +0200

    labor2

--- labor-01/src/oop/labor01/Main.java
+++ labor-01/src/oop/labor01/Main.java
@@ -3,8 +3,12 @@ package oop.labor01;
 public class Main {
     public static void main(String[] args) {
         String nev = "Csipor Antal";
-        for (int i=0; i<nev.lenght();++i){
+        for (int i=0; i<nev.length();++i){
             System.out.println(nev.charAt(i));
+        }
+        String ama ="ALMAFA";
+        for (int i=0; i<ama.length();++i){
+            System.out.println(i);
         }
 
     }
--- labor-02/src/oop/labor02/Main.java
+++ labor-02/src/oop/labor02/Main.java
@@ -1,7 +1,38 @@
 package oop.labor02;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("hi");
+public class Main{
+
+    public static void main(String[] args){
+        //1
+//        Bankszamla szamla = new Bankszamla( 1000 );
+//        System.out.println( szamla.getEgyenleg() );
+//        szamla.betesz( 500 );
+//        szamla.kivesz( 2000 );
+//        System.out.println( szamla.getEgyenleg() );
+//        szamla.kivesz( 2000 );
+//        System.out.println( szamla.getEgyenleg() );
+
+        //2
+//          Auto kekauto = new Auto("kek");
+//          for (int i=0; i<=11;++i){
+//          kekauto.novelSebesseg(); }
+//          System.out.println(kekauto);
+//          Auto sargaauto = new Auto("sarga");
+//          for (int i=0; i<=5;++i){
+//          sargaauto.novelSebesseg(); }
+//          System.out.println(sargaauto);
+          //3
+           Datum datum = new Datum(2022 ,2, 24);
+           System.out.println(datum);
+
+
+
+
+
+
+
+
+        }
+
     }
-}
+

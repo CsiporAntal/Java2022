1de3cfba7b6a5aba976ce5f1849a67915c4d57f4
CsiporAntal <99884681+CsiporAntal@users.noreply.github.com>
2022-05-12 16:08:31 +0300

    labor12

--- labor-10/src/oop/labor10/Main.java
+++ labor-10/src/oop/labor10/Main.java
@@ -1,7 +1,20 @@
 package oop.labor10;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("hi");
+        double sum = 0;
+        try (Scanner sc = new Scanner(System.in)) {
+            String line = sc.nextLine();
+            for (String s : line.split(" ")) {
+                try {
+                    sum += Double.parseDouble(s.trim());
+                } catch (NumberFormatException ignore){  }
+            }
+            
+            System.out.println(sum);
+
+        }
     }
 }
